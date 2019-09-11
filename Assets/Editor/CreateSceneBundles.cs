ea70854b8d8b95d75cc4b09fa41853e00b496c70
Paul Sinnett <paul.sinnett@gmail.com>
2019-09-12 00:57:22 +0100

    Initial commit

---
+++ Assets/Editor/CreateSceneBundles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateSceneBundles : MonoBehaviour
+{
+	[MenuItem("Scene Bundles/Build")]
+	static void Build()
+	{
+		BuildAssetBundleOptions options = BuildAssetBundleOptions.None;
+		BuildTarget target = BuildTarget.StandaloneWindows64;
+		BuildPipeline.BuildAssetBundles("SceneBundles", options, target);
+	}
+}
---
+++ Assets/LoadSceneFromBundle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneFromBundle : MonoBehaviour
+{
+	public Text log;
+	public string bundleName;
+	public string sceneName;
+
+	IEnumerator Start()
+	{
+		yield return new WaitForSeconds(5);
+
+		string path = 
+			Path.Combine(
+				Path.Combine(
+					Path.GetDirectoryName(Application.dataPath),
+					"SceneBundles"),
+				bundleName);
+
+		log.text = string.Format("Loading scene bundle {0}", path);
+		yield return new WaitForSeconds(1);
+
+		AssetBundleCreateRequest request =
+			AssetBundle.LoadFromFileAsync(path);
+
+		yield return request;
+		yield return SceneManager.LoadSceneAsync(sceneName);
+	}
+}
